95b1ab42bb8be4bc047179e7c06f581da0695652
Ladislav Hovan <19410951+ladislav-hovan@users.noreply.github.com>
2024-05-29 14:42:03 +0200

    Fix the bug, refactor

--- tests/test_filter_pathways.py
+++ tests/test_filter_pathways.py
@@ -5,7 +5,7 @@ sys.path.append('./')
 from init import *
 
 def test_filter_pathways():
-    _, edges = load_data('80_tcga_lms_net.RData', 'rand_genes.RData')
+    edges = load_edges_file('rand_genes.RData')
     pathways = load_gmt('c2.cp.reactome.v7.1.symbols.gmt')
     pathways_filt = filter_pathways(pathways, edges)
     assert len(pathways_filt) == 1042  # R result
