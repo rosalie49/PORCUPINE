bf283153aa6bfc0918a3f67caf9609e4a2886b94
rosalie49 <rosalie.coulon@hotmail.fr>
2024-05-30 11:06:59 +0200

    add test_filter_pathways_size

--- tests/test_filter_pathways.py
+++ tests/test_filter_pathways.py
@@ -9,3 +9,10 @@ def test_filter_pathways():
     pathways = load_gmt('c2.cp.reactome.v7.1.symbols.gmt')
     pathways_filt = filter_pathways(pathways, edges)
     assert len(pathways_filt) == 1042  # R result
+
+def test_filter_pathways_size():
+    edges = load_edges_file('rand_genes.RData')
+    pathways = load_gmt('c2.cp.reactome.v7.1.symbols.gmt')
+    pathways_filt = filter_pathways(pathways, edges)
+    pathways_filt_size = filter_pathways_size(pathways_filt,minSize=5,maxSize=150)
+    assert len(pathways_filt_size) == 210 # R result 
