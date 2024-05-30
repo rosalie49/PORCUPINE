45a995f38ee59981c2fb11cb6f41637ce3548f7e
rosalie49 <rosalie.coulon@hotmail.fr>
2024-05-30 15:23:44 +0200

    reduce functions size

--- tests/test_filter_pathways.py
+++ tests/test_filter_pathways.py
@@ -4,15 +4,16 @@ sys.path.append('./')
 
 from init import *
 
+edges = load_edges_file('rand_genes.RData')
+pathways = load_gmt('c2.cp.reactome.v7.1.symbols.gmt')
+
 def test_filter_pathways():
-    edges = load_edges_file('rand_genes.RData')
-    pathways = load_gmt('c2.cp.reactome.v7.1.symbols.gmt')
     pathways_filt = filter_pathways(pathways, edges)
     assert len(pathways_filt) == 1042  # R result
 
+    
+
 def test_filter_pathways_size():
-    edges = load_edges_file('rand_genes.RData')
-    pathways = load_gmt('c2.cp.reactome.v7.1.symbols.gmt')
     pathways_filt = filter_pathways(pathways, edges)
     pathways_filt_size = filter_pathways_size(pathways_filt,minSize=5,maxSize=150)
     assert len(pathways_filt_size) == 210 # R result 
