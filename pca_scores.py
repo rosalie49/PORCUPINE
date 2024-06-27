849d830a604f550640af21d2edd86648d8194db7
rosalie49 <rosalie.coulon@hotmail.fr>
2024-06-27 14:52:16 +0200

    add get_features_scores

--- pca_scores.py
+++ pca_scores.py
@@ -14,7 +14,8 @@ def get_ind_scores(data, scale_data=True, center_data=True):
     Returns:
         pd.DataFrame: patient heterogeneity scores for PC1 and PC2
     """
-    data_t = np.transpose(data)
+    #Transpose data
+    data_t =np.transpose(data)
 
     # Scale and/or center the data if specified
     if scale_data or center_data:
@@ -53,7 +54,7 @@ def get_pathway_ind_scores(pathways_list, reg_net, edges, scale_data=True, cente
         subnet = reg_net.loc[idx]
         # Perform PCA analysis on the subset
         individual_scores = get_ind_scores(subnet, scale_data=scale_data, center_data=center_data)
-        # Store the results in a DataFrame with patient as index, a pathway column and scores of the first two principal components
+        # Store the results in a DataFrame with the patient as index, a pathway column and scores of the first two principal components
         res_pca = pd.DataFrame({'pathway':pathway.iloc[0], 'pc1':individual_scores['pc1'], 'pc2':individual_scores['pc2']})
         res_pca.index = individual_scores.index
         res.append(res_pca)
@@ -61,3 +62,45 @@ def get_pathway_ind_scores(pathways_list, reg_net, edges, scale_data=True, cente
     res_all = pd.concat(res)   
 
     return res_all
+
+def get_features_scores(data, pathway_name, reg, tar, scale_data = True, center_data = True):
+    # Transpose data
+    data_t = np.transpose(data)
+
+    # Scale and/or center the data if specified
+    if scale_data or center_data:
+        # Initialize the StandardScaler object with mean centering and scaling
+        scaler = StandardScaler(with_mean=center_data, with_std=scale_data)
+        # Scale and center the data using the scaler object
+        data_t = scaler.fit_transform(data_t)
+    
+    # Perform PCA
+    pca = PCA(n_components=2)
+    pca.fit(data_t)
+    
+    # Extract principal components (rotation matrix)
+    edges_scores = pca.components_.T[:, :2]
+    
+    # Convert to DataFrame and add pathway, reg, and tar columns
+    edges_scores = pd.DataFrame(edges_scores, columns=['PC1', 'PC2'])
+    edges_scores['pathway'] = pathway_name
+    edges_scores['reg'] = reg.reset_index(drop=True)
+    edges_scores['tar'] = tar.reset_index(drop=True)
+
+    return edges_scores
+
+def get_pathway_features_scores(pathways_list, reg_net, edges, scale_data= True, center_data = True):
+    res = []
+    for _, pathway in pathways_list.iterrows():
+        print(pathway.iloc[0])
+        pathway_name = pathway.iloc[0]
+        all_genes = pathway['genes']
+        idx = edges[edges['tar'].isin(all_genes)].index
+        reg = edges.loc[edges['tar'].isin(all_genes), 'reg']
+        tar = edges.loc[edges['tar'].isin(all_genes), 'tar']   
+        # Extract the subset of the regulatory network corresponding to these indices
+        subnet = reg_net.loc[idx]
+        edges_scores = get_features_scores(subnet, pathway_name, reg, tar, scale_data = scale_data, center_data = center_data)
+        res.append(edges_scores)
+
+    return res 
