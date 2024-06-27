3998c9183f938ba7de9a3d970ddb8c7a436a6aff
rosalie49 <rosalie.coulon@hotmail.fr>
2024-06-27 15:17:45 +0200

    add docstrings and hints

--- pca_scores.py
+++ pca_scores.py
@@ -7,7 +7,7 @@ def get_ind_scores(data, scale_data=True, center_data=True):
     """This function extracts patient heterogeneity scores on the first two principal components.
 
     Args:
-        data (table): numeric matrix with samples in columns and features in rows
+        data (pd.DataFrame): numeric matrix with samples in columns and features in rows
         scale_data (bool, optional): Whether to scale the data (TRUE) or not (FALSE). Defaults to True.
         center_data (bool, optional): Whether to center the data (TRUE) or not (FALSE). Defaults to True.
 
@@ -64,6 +64,19 @@ def get_pathway_ind_scores(pathways_list, reg_net, edges, scale_data=True, cente
     return res_all
 
 def get_features_scores(data, pathway_name, reg, tar, scale_data = True, center_data = True):
+    """This function extracts feature scores on the first two principal components
+
+    Args:
+        data (pd.DataFrame): Numeric matrix with samples in columns, and features in rows
+        pathway_name (str): Name of the pathway
+        reg (pd.Series): regulatory elements associated with the target features
+        tar (pd.Series): target features
+        scale_data (bool, optional): Whether to scale the data (TRUE) or not (FALSE). Defaults to True.
+        center_data (bool, optional):  Whether to center the data (TRUE) or not (FALSE). Defaults to True.
+
+    Returns:
+        pd.DataFrame:  features scores on the PC1 and PC2
+    """
     # Transpose data
     data_t = np.transpose(data)
 
@@ -90,17 +103,35 @@ def get_features_scores(data, pathway_name, reg, tar, scale_data = True, center_
     return edges_scores
 
 def get_pathway_features_scores(pathways_list, reg_net, edges, scale_data= True, center_data = True):
+    """This functions extracts feature scores on the first two principal components for a list of pathways
+
+    Args:
+        pathways_list (pd.DataFrame): a list of pathways with associated genes
+        reg_net (pd.DataFrame): Numeric matrix with samples in columns, features in rows
+        edges (pd.DataFrame): containing information on "reg" and "tar"
+        scale_data (bool, optional): Whether to scale the data (TRUE) or not (FALSE). Defaults to True.
+        center_data (bool, optional): Whether to center the data (TRUE) or not (FALSE). Defaults to True.
+
+    Returns:
+        list: edges scores on PC1 and PC2 for each pathway
+    """
+    # Initialize an empty list to store results for each pathway
     res = []
+    # Iterate over each pathway in the pathways list
     for _, pathway in pathways_list.iterrows():
-        print(pathway.iloc[0])
+        # Extract the pathway name
         pathway_name = pathway.iloc[0]
+        # Identify the indices of edges where the target genes are in the pathway's gene list
         all_genes = pathway['genes']
         idx = edges[edges['tar'].isin(all_genes)].index
+        # Extract the corresponding regulatory elements and target features
         reg = edges.loc[edges['tar'].isin(all_genes), 'reg']
         tar = edges.loc[edges['tar'].isin(all_genes), 'tar']   
         # Extract the subset of the regulatory network corresponding to these indices
         subnet = reg_net.loc[idx]
+        # Perform PCA analysis on the subset
         edges_scores = get_features_scores(subnet, pathway_name, reg, tar, scale_data = scale_data, center_data = center_data)
+        # Append the resulting DataFrame to the results list
         res.append(edges_scores)
 
     return res 
