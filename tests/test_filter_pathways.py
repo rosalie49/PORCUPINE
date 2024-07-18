0ef90ac41bf24567f345b9d5aca6063b470c557a
rosalie49 <rosalie.coulon@hotmail.fr>
2024-07-18 15:11:17 +0200

    changed data and results

--- tests/test_filter_pathways.py
+++ tests/test_filter_pathways.py
@@ -4,16 +4,27 @@ sys.path.append('./')
 
 from porcupine.init import *
 
-edges = load_edges_file('rand_genes.RData')
-pathways = load_gmt('c2.cp.reactome.v7.1.symbols.gmt')
+
+edges_data = {
+    'reg': ['geneA', 'geneB', 'geneC', 'geneD', 'geneE'],
+    'tar': ['gene1', 'gene2', 'gene3', 'gene4', 'gene5']
+}
+edges = pd.DataFrame(edges_data)
+
+pathways = [
+    ('pathway1', ['gene1', 'gene2', 'gene3', 'gene4','gene5']),
+    ('pathway2', ['gene2', 'gene3', 'gene4','gene9']),
+    ('pathway3', ['gene4', 'gene5']),
+    ('pathway4', ['gene10', 'gene11'])
+]
 
 def test_filter_pathways():
     pathways_filt = filter_pathways(pathways, edges)
-    assert len(pathways_filt) == 1042  # R result
+    assert len(pathways_filt) == 3  
 
     
 
 def test_filter_pathways_size():
     pathways_filt = filter_pathways(pathways, edges)
-    pathways_filt_size = filter_pathways_size(pathways_filt,minSize=5,maxSize=150)
-    assert len(pathways_filt_size) == 210 # R result 
+    pathways_filt_size = filter_pathways_size(pathways_filt,minSize=3,maxSize=4)
+    assert len(pathways_filt_size) == 1
