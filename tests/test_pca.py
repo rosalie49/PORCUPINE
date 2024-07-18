2f2674b04c6f805753c8e261e2c2d554f87d7795
rosalie49 <rosalie.coulon@hotmail.fr>
2024-07-18 13:56:33 +0200

    changed test result

--- tests/test_pca.py
+++ tests/test_pca.py
@@ -19,15 +19,15 @@ def test_run_pca():
 
 def test_process_pathway():
     net_data = data_df
-    pathway_data = {'pathway':'PATHWAY1', 'genes' : ['gene1','gene2','gene3','gene4','gene5']}
+    pathway_data = pd.Series({'pathway':'PATHWAY1', 'genes' : ['gene1','gene2','gene3','gene4','gene5']})
     edges_data = {
     'reg': ['geneA', 'geneB', 'geneC', 'geneD', 'geneE'], 
-    'tar': ['gene1', 'gene2', 'gene3', 'gene4', 'gene5']}
+    'tar': ['gene1', 'gene2', 'gene3', 'gene4', 'gene6']}
     edges_df = pd.DataFrame(edges_data)
     
     res = process_pathway(pathway_data,net_data, edges_df)
 
-    assert res[1] == pytest.approx(56.856, abs=0.001) # R result 
-    assert res[2] == 5 #R result
-    assert res[3] == 5 #R result 
+    assert res[1] == pytest.approx(48.73503, abs=0.001) #R result
+    assert res[2] == 4 #R result
+    assert res[3] == 5 #R result
 
